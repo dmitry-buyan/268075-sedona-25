953faae80afb2c096dbd3ed865e6f9b0be2a16eb
Dmitry Buyan <buyandl@mail.ru>
2019-06-13 22:00:34 +0400

    Теперь форма трясетка каждый раз, когда нажимаешь кнопку Найти с пустыми полями

--- js/script.js
+++ js/script.js
@@ -8,12 +8,15 @@ var childrenAmount = popup.querySelector("#children");
 button.addEventListener("click", function(evt) {
   evt.preventDefault();
   popup.classList.toggle("search__form--disable");
+  popup.classList.remove("search__form--error");
   arrival.focus();
 });
 
 popup.addEventListener("submit", function(evt) {
   if (!arrival.value || !leave.value || !adultAmount.value || !childrenAmount.value) {
     evt.preventDefault();
+    popup.classList.remove("search__form--error");
+    popup.offsetWidth = popup.offsetWidth;
     popup.classList.add("search__form--error");
   }
 });
